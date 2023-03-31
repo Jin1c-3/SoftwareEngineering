ad82f16097f0e147cef701fdc2102ec3e53f04e3
lzc <1649943136@qq.com>
2023-03-31 22:46:43 +0800

    规范表名、格式等
    --看注释

--- db/sep_lzc.sql
+++ db/sep_lzc.sql
@@ -1,72 +1,70 @@
-
 ----管理员表  00001为root管理员
-create table manager(
-		manager_ID char(5) primary key,
-		manager_name nvarchar(20),
-		manager_pwd varchar(30) check (len(manager_pwd) between 6 and 30)
+create table super_usr(
+	super_usr_ID char(5) primary key,
+	super_usr_name nvarchar(20),
+	super_usr_pwd varchar(30) check (len(super_usr_pwd) between 6 and 30)
 )
 ----服务商表
-create table SerProviders(
-	Ser_ID int primary key identity(1,1),
-	Ser_pwd varchar(30) check (len(Ser_pwd) between 6 and 30),
-	Ser_name nvarchar(30),
-	profit float
+create table service_provider(
+	service_provider_ID int primary key identity(1,1),
+	service_provider_pwd varchar(30) check (len(service_provider_pwd) between 6 and 30),	--密码6-30位
+	service_provider_name nvarchar(30),
+	push_money float	--约定提成
 )
 ----飞机信息表
-create table Aircraft(
-	Air_ID varchar(20) primary key,
-	Air_type varchar(20),
-	Air_T_num int,
-	Air_M_num int,
-	Air_L_num int,
-	Air_status nvarchar(3) check (Air_status = '可用' or Air_status = '不可用' ) default('可用'),
-	Ser_ID int ,
-	foreign key(Ser_ID)  references SerProviders(Ser_ID)
+create table aircraft(
+	aircraft_ID varchar(20) primary key,
+	aircraft_type varchar(20),
+	aircraft_T_num int,	--头等舱
+	aircraft_M_num int,	--商务舱
+	aircraft_L_num int,	--经济舱
+	aircraft_status nvarchar(3) check (aircraft_status = '可用' or aircraft_status = '不可用' ) default('可用'),
+	service_provider_ID int ,
+	foreign key(service_provider_ID)  references service_provider(service_provider_ID)
 )
 ----航班信息表
-create table F_Info(
+create table flight_info(
 	--班期
 	 T varchar(18),
-	 f_ID varchar(25) primary key,
-	 f_st_city nvarchar(10),
-	 f_en_city nvarchar(10),
-	 f_st_port nvarchar(10),
-	 f_en_port nvarchar(10),
-	 f_status nvarchar(20),
-	 Air_type varchar(20),
-	 zhifei nvarchar(2) check(zhifei = '是' or zhifei = '否'),
-	 Air_ID varchar(20),
-	 foreign key(Air_ID) references Aircraft(Air_ID)
+	 flight_ID varchar(25) primary key,
+	 flight_start_city nvarchar(10),
+	 flight_end_city nvarchar(10),
+	 flight_start_port nvarchar(10),
+	 flight_end_port nvarchar(10),
+	 flight_status nvarchar(20),
+	 aircraft_type varchar(20),
+	 direct_flag nvarchar(2) check(direct_flag = '是' or direct_flag = '否'),
+	 aircraft_ID varchar(20),
+	 foreign key(aircraft_ID) references Aircraft(aircraft_ID)
 )
 ----飞机行程信息表
-create table F_time(
-	 f_st_time datetime,
-	 f_en_time datetime,
-	 f_ID varchar(25),
+create table flight_time(
+	 flight_start_time datetime,   
+	 flight_end_time datetime,
+	 flight_ID varchar(25),
 	 ----航班号  外键
-	 f_st_city nvarchar(10),
-	 f_en_city nvarchar(10),
-	 f_st_port nvarchar(10),
-	 f_en_port nvarchar(10),
+	 flight_start_city nvarchar(10),
+	 flight_end_city nvarchar(10),
+	 flight_start_port nvarchar(10),
+	 flight_end_port nvarchar(10),
 	 --站序
-	 f_order int default(1),
-	 f_price int,
-	  foreign key (f_ID) references F_Info(f_ID)
+	 flight_order int default(1),
+	 flight_price int,
+	  foreign key (flight_ID) references flight_info(flight_ID)
 )
 
------
 ---用户
 create table usr(
-	usr_account varchar(20) primary key,  --注册时随机分配
-	usr_ID varchar(20) unique,   
-	usr_email varchar(35) unique,
+	usr_account varchar(20) primary key,  --用户账号 注册时随机分配
+	usr_ID varchar(20) unique,   --uuid
+	usr_email varchar(35) unique,  
 	usr_avater varchar(70) unique, --存头像地址
 	usr_viplevel int,
 	usr_pwd varchar(30)
 )
 
 
-
+--总订单表 包括飞机火车
 create table whole_order(
 	order_ID int identity(1,1) primary key,--订单编号
 	order_time datetime,
@@ -75,23 +73,22 @@ create table whole_order(
 	usr_ID varchar(20) 
 )
 
-create table f_ticket(
+create table flight_ticket(
 	  ---主键
 	 ticket_ID int identity(1,1) primary key,
 	 --外键
-	 f_ID varchar(25),
+	 flight_ID varchar(25),
 	 order_ID int,--订单编号  自增
 
-	 f_st_time datetime,
-	 f_en_time datetime,
+	 flight_start_time datetime,
+	 flight_end_time datetime,
 	 ticket_status nvarchar(10),
 	 passenger_name nvarchar(15),
 	 seat_type nvarchar(10),
-	 f_st_port nvarchar(10),
-	 f_en_port nvarchar(10),
-	 f_price int,
-	 foreign key(f_ID) references F_Info(f_ID),
+	 flight_start_port nvarchar(10),
+	 flight_end_port nvarchar(10),
+	 flight_price int,
+	 foreign key(flight_ID) references flight_info(flight_ID),
 	 foreign key(order_ID) references whole_order(order_ID)
-
 )
 
